8868b28beff6485b1320c9289703e61c01d91b21
Kevin <kevhines@gmail.com>
2021-01-26 10:43:39 -0500

    commit tree 47473f66f85cfc7fdddd38ca801f957cf02c47a4

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+
+    attr_reader :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def backers
+        ProjectBacker.all.map do |pb|
+            pb.backer if pb.project == self
+        end.compact
+    end
+end
